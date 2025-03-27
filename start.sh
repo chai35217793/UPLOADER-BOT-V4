8a05d057522042de016b87f8095c215e4097fe2b
chai35217793 <149489960+chai35217793@users.noreply.github.com>
2025-03-27 21:50:00 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-# Start the bot script first
-python3 bot.py &
-
-# Wait for a few seconds to let the bot initialize
-sleep 5
-
-# Start Flask app with Gunicorn on port 8000
-gunicorn --bind 0.0.0.0:8000 app:app
+gunicorn app:app & python3 bot.py
